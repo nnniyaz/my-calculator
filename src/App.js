6fa27de3435746bc928eb25c3294e07a7c5a4fa0
Niyaz Nassyrov <nassyrovich@gmail.com>
2022-08-06 18:59:38 +0600

    Finished with icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 
+// svgs
+import plus from './assets/plus-solid.svg'
+import minus from './assets/minus-solid.svg'
+import equal from './assets/equals-solid.svg'
+import divide from './assets/divide-solid.svg'
+import plusMinus from './assets/plus-minus-solid.svg'
+import multiply from './assets/xmark-solid.svg'
+import percent from './assets/percent-solid.svg'
+
 function App() {
   const [firstValue, setFirstValue] = useState('');
   const [prevValue, setPrevValue] = useState('');
@@ -92,32 +101,46 @@ function App() {
         <div className='keyboard'>
           <div className='row'>
             <div className='block light-gray' onClick={clear}>AC</div>
-            <div className='block light-gray' onClick={changeSign}>+/-</div>
-            <div className='block light-gray' onClick={inPercentage}>%</div>
-            <div className='block orange' onClick={setOperator}>÷</div>
+            <div className='block light-gray' onClick={changeSign}>
+              <img src={plusMinus} alt="icon" />
+            </div>
+            <div className='block light-gray' onClick={inPercentage}>
+              <img src={percent} alt="icon" />
+            </div>
+            <div className='block orange' onClick={setOperator}>
+              <img src={divide} alt="icon" />
+            </div>
           </div>
           <div className='row'>
             <div className='block dark-gray' onClick={setValue}>7</div>
             <div className='block dark-gray' onClick={setValue}>8</div>
             <div className='block dark-gray' onClick={setValue}>9</div>
-            <div className='block orange' onClick={setOperator}>x</div>
+            <div className='block orange' onClick={setOperator}>
+              <img src={multiply} alt="icon" />
+            </div>
           </div>
           <div className='row'>
             <div className='block dark-gray' onClick={setValue}>4</div>
             <div className='block dark-gray' onClick={setValue}>5</div>
             <div className='block dark-gray' onClick={setValue}>6</div>
-            <div className='block orange' onClick={setOperator}>-</div>
+            <div className='block orange' onClick={setOperator}>
+              <img src={minus} alt="icon" />
+            </div>
           </div>
           <div className='row'>
             <div className='block dark-gray' onClick={setValue}>1</div>
             <div className='block dark-gray' onClick={setValue}>2</div>
             <div className='block dark-gray' onClick={setValue}>3</div>
-            <div className='block orange' onClick={setOperator}>+</div>
+            <div className='block orange' onClick={setOperator}>
+              <img src={plus} alt="icon" />
+            </div>
           </div>
           <div className='row'>
             <div className='block dark-gray' onClick={setValue}>0</div>
             <div className='block dark-gray' onClick={setFloat}>.</div>
-            <div className='equal orange' onClick={equals}>=</div>
+            <div className='equal orange' onClick={equals}>
+              <img src={equal} alt="icon" />
+            </div>
           </div>
         </div>
       </div>
